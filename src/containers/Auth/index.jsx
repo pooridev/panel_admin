5d4ba22f1b96913c4feb6c6a17fc1648de514aa5
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-07-26 14:00:14 +0430

    [update] redirect user to the home page, after submiting the login form

--- src/containers/Auth/index.jsx
+++ src/containers/Auth/index.jsx
@@ -1,14 +1,21 @@
 import Form from '../../components/LoginForm';
 import Header from './Header/index';
 import styles from './Auth.module.css';
+import { Redirect } from 'react-router-dom';
+import { useAuth } from './../../providers/auth-context';
 
-const Auth = () => (
-  <div className={styles.Auth}>
-    <div className='max-w-md w-full space-y-8'>
-      <Header />
-      <Form />
+const Auth = () => {
+  const { userData } = useAuth();
+  // Will redirect user to the dashboard page if he/she was not logged in
+  if (userData) return <Redirect to='/' />;
+
+  return (
+    <div className={styles.Auth}>
+      <div className='max-w-md w-full space-y-8'>
+        <Header />
+        <Form />
+      </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default Auth;
